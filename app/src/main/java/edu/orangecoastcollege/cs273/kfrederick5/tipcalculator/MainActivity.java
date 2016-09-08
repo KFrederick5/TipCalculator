f68fbe1194fb9de8d9203e04160bfc711da2c8cc
kfrederick5 <tefsuncle@gmail.com>
2016-09-08 16:37:13 -0700

    Added doc information

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tipcalculator/MainActivity.java
@@ -2,12 +2,70 @@ package edu.orangecoastcollege.cs273.kfrederick5.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Associate the controller with the needed views
+    private EditText amountEditText;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private SeekBar percentSeekBar;
+    private TextView tipTextView;
+    private TextView totalTextView;
+
+    //Associate the controller with needed model
+    RestaurantBill currentBill = new RestaurantBill();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Connect the controller with widgets in our app
+        amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        //Define a listener for the amountEditText onTextChange
+        amountEditText.addTextChangedListener(amountTextChangedListener);
+
+
+
+
+    }
+
+    private TextWatcher amountTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            //Do nothing
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            //Try to get the amount from amountEditText:
+            try{
+                double amount = Double.parseDouble(charSequence.toString()) / 100.0;
+                currentBill.setAmount(amount);
+            }
+            catch(NumberFormatException e)
+            {
+                amountEditText.setText("");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            //Do nothing
+        }
     }
 }
+
+
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/tipcalculator/RestaurantBill.java
@@ -0,0 +1,56 @@
+package edu.orangecoastcollege.cs273.kfrederick5.tipcalculator;
+
+/**
+ * Created by kfrederick5 on 9/8/2016.
+ */
+public class RestaurantBill {
+    private double mAmount;
+    private double mTipPercent;
+    private double mTipAmount;
+    private double mTotalAmount;
+
+    public RestaurantBill() {
+        mAmount = 0.0;
+        mTipPercent = 0.0;
+        mTipAmount = 0.0;
+        mTotalAmount = 0.0;
+    }
+
+    public RestaurantBill(double mAmount, double mTipPercent) {
+        this.mAmount = mAmount;
+        this.mTipPercent = mTipPercent;
+        recalcAmounts();
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public double getTipPercent() {
+        return mTipPercent;
+    }
+
+    public double getTipAmount() {
+        return mTipAmount;
+    }
+
+    public double getTotalAmount() {
+        return mTotalAmount;
+    }
+
+    public void setAmount(double mAmount) {
+        this.mAmount = mAmount;
+        recalcAmounts();
+    }
+
+    public void setTipPercent(double mTipPercent) {
+        this.mTipPercent = mTipPercent;
+        recalcAmounts();
+    }
+
+    private void recalcAmounts()
+    {
+        mTipAmount = mAmount*mTipPercent;
+        mAmount += mTipAmount;
+    }
+}
